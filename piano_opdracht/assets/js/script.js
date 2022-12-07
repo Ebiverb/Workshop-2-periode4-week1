beef6741c18be3c504b875743db44565b4316e87
Ebi <ebiverbeek@gmail.com>
2022-12-07 14:02:31 +0100

    first commit

---
+++ piano_opdracht/assets/js/script.js
@@ -0,0 +1,123 @@
+document.getElementById("a").onclick = function(){ // als op de c-toets wordt geklikt
+	var a_toets = document.getElementById("audio_a"); // zoek het c-toets audio-element op
+	a_toets.play(); // speel het audio-element af
+}
+document.getElementById("b").onclick = function(){
+	var b_toets = document.getElementById("audio_b");
+	b_toets.play();
+}
+document.getElementById("c").onclick = function(){
+	var c_toets = document.getElementById("audio_c");
+	c_toets.play();
+}
+
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+
+document.getElementById("f").onclick = function(){
+	var f_toets = document.getElementById("audio_f");
+	f_toets.play();
+}
+
+document.getElementById("g").onclick = function(){
+	var g_toets = document.getElementById("audio_g");
+	g_toets.play();
+}
+
+document.getElementById("h").onclick = function(){
+	var h_toets = document.getElementById("audio_h");
+	h_toets.play();
+}
+
+document.getElementById("i").onclick = function(){
+	var i_toets = document.getElementById("audio_i");
+	i_toets.play();
+}
+
+document.getElementById("j").onclick = function(){
+	var j_toets = document.getElementById("audio_j");
+	j_toets.play();
+}
+document.getElementById("i").onclick = function(){
+	var i_toets = document.getElementById("audio_i");
+	i_toets.play();
+}
+document.getElementById("j").onclick = function(){
+	var j_toets = document.getElementById("audio_j");
+	j_toets.play();
+}
+
+document.getElementById("k").onclick = function(){
+	var k_toets = document.getElementById("audio_k");
+	k_toets.play();
+}
+document.getElementById("l").onclick = function(){
+	var l_toets = document.getElementById("audio_l");
+	l_toets.play();
+}
+document.getElementById("m").onclick = function(){
+	var m_toets = document.getElementById("audio_m");
+	m_toets.play();
+}
+document.getElementById("n").onclick = function(){
+	var n_toets = document.getElementById("audio_n");
+	n_toets.play();
+}
+document.getElementById("o").onclick = function(){
+	var o_toets = document.getElementById("audio_o");
+	o_toets.play();
+}
+document.getElementById("p").onclick = function(){
+	var p_toets = document.getElementById("audio_p");
+	p_toets.play();
+}
+document.getElementById("q").onclick = function(){
+	var q_toets = document.getElementById("audio_q");
+	q_toets.play();
+}
+document.getElementById("r").onclick = function(){
+	var r_toets = document.getElementById("audio_r");
+	r_toets.play();
+}
+document.getElementById("s").onclick = function(){
+	var s_toets = document.getElementById("audio_s");
+	s_toets.play();
+}
+document.getElementById("t").onclick = function(){
+	var t_toets = document.getElementById("audio_t");
+	t_toets.play();
+}
+document.getElementById("u").onclick = function(){
+	var u_toets = document.getElementById("audio_u");
+	u_toets.play();
+}
+document.getElementById("v").onclick = function(){
+	var v_toets = document.getElementById("audio_v");
+	v_toets.play();
+}
+document.getElementById("w").onclick = function(){
+	var w_toets = document.getElementById("audio_w");
+	w_toets.play();
+}
+document.getElementById("x").onclick = function(){
+	var x_toets = document.getElementById("audio_x");
+	x_toets.play();
+}
+
+
+
+
+
+
+
+
+
+
+
